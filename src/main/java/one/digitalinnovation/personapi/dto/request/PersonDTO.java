3b3d576a9b5ec7b980f8f88e4915b1d6b292eba4
Diego Peixoto <diegodop@gmail.com>
2021-10-23 18:02:43 -0300

    Projeto Concluído

--- src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
@@ -13,7 +13,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import one.digitalinnovation.personapi.entity.Phone;
 
 @Data
 @Builder
